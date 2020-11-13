24c7cd193dd8bc7d325bb479a7e54150dfb7f703
Igbalajobi Zhikrullah <zalajobi@gmail.com>
2020-11-13 07:16:05 +0100

    quick fixes

--- component/Header.js
+++ component/Header.js
@@ -1,20 +1,36 @@
 import React, {Component} from 'react'
 import Head from "next/head";
-import {Form, FormControl, Nav, Navbar} from "react-bootstrap";
-import Button from "react-bootstrap/Button";
 // import $ from 'jquery';
 // import Popper from 'popper.js';
 // import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 export default class Header extends Component {
+
+    state = {
+        color: "transparent"
+    }
+
+    listenScrollEvent = e => {
+        if (window.scrollY > 100) {
+            this.setState({color: 'white'})
+        } else if (window.screenY < 100){
+            this.setState({color: 'transparent'})
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.listenScrollEvent)
+    }
+
+
     render() {
         return (
             <React.Fragment>
-                <nav className="navbar navbar-expand-lg navbar-light bg-transparent fixed-top">
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top" style={{backgroundColor: this.state.color}}>
                     <div className="container">
                         {/*<a className="navbar-brand" href="#">Start Bootstrap</a>*/}
-                        <a className="navbar-brand" href="#">
-                            <img src="https://res.cloudinary.com/zalajobi/image/upload/v1605233607/Zhik%20Tours/Logo/logo_transparent_wetmtj.png" style={{height: "100px", width: "100px"}}/>
+                        <a className="navbar-brand" href="/">
+                            <img src="https://res.cloudinary.com/zalajobi/image/upload/v1605233607/Zhik%20Tours/Logo/logo_transparent_wetmtj.png" style={{height: "80px", width: "100px"}}/>
                         </a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse"
                                 data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
@@ -23,19 +39,26 @@ export default class Header extends Component {
                         </button>
                         <div className="collapse navbar-collapse" id="navbarResponsive">
                             <ul className="navbar-nav ml-auto">
-                                <li className="nav-item active">
-                                    <a className="nav-link" href="#">Home
-                                        <span className="sr-only">(current)</span>
+                                <li className="nav-item m-auto">
+                                    <a className="nav-link" href="/">home
+                                        <span className="sr-only"/>
                                     </a>
                                 </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">About</a>
+
+                                <li className="nav-item m-auto">
+                                    <a className="nav-link" href="#">about us</a>
                                 </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Services</a>
+
+                                <li className="nav-item m-auto">
+                                    <a className="nav-link" href="#">top destinations</a>
                                 </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Contact</a>
+
+                                <li className="nav-item m-auto">
+                                    <a className="nav-link" href="#">news</a>
+                                </li>
+
+                                <li className="nav-item m-auto">
+                                    <a className="nav-link" href="#">contact us</a>
                                 </li>
                             </ul>
                         </div>
@@ -44,9 +67,11 @@ export default class Header extends Component {
 
 
                 <Head>
+                    <link rel="icon" type="image/png" href="https://res.cloudinary.com/zalajobi/image/upload/v1605233607/Zhik%20Tours/Logo/logo_transparent_wetmtj.png"/>
                     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossOrigin="anonymous"/>
                     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossOrigin="anonymous"/>
                     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossOrigin="anonymous"/>
+                    <script src="main.js"/>
                 </Head>
             </React.Fragment>
         )
---
+++ public/main.js
@@ -0,0 +1,34 @@
+// // var header = $("#header");
+// // // var x = window.matchMedia("(max-width: 700px)")
+// //
+// // $(document).scroll(function(e) {
+// //     if($(this).scrollTop() > $("#banner").height()) {
+// //         header.css({"position" : "fixed", "top" : "0", "background-color" : "white"});
+// //     } else {
+// //         header.css("position", "relative");
+// //     }
+// // });
+// // var header = document.getElementById('header');
+//
+// //
+// // $(document).ready(function(){
+// //     var scroll_pos = 0;
+// //     $(document).scroll(function() {
+// //         scroll_pos = $(this).scrollTop();
+// //         if(scroll_pos > 300) {
+// //             $(header).css('background-color', 'white !important');
+// //         } else {
+// //             $(header).css('background-color', 'transparent !important');
+// //         }
+// //     });
+// // });
+//
+// // window.addEventListener('scroll', function() {
+// //     if (window.screenY === 400) {
+// //         document.getElementsByClassName('header').style.backgroundColor = 'white';
+// //     }
+// // });
+//
+// function changeBackground() {
+//     console.log("Check");
+// }
